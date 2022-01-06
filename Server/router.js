a1a531e6e3335e43ec2dba031aba79aaf8b163d4
Natasha <natashajvandam@outlook.com>
2022-01-06 10:38:34 +0100

    notes

--- Server/router.js
+++ Server/router.js
@@ -6,10 +6,12 @@ import {fetchTeam, createNewTeam, addRider, removeRider} from './controllers/use
 
 //router.get('/allriders', fetchRiderData); 
 //---needs to return just name, (teamname?), price, whether on roster?, (current points?)
+
 router.post('/newTeam', createNewTeam); //{ username, team, password} = req.body;
-router.get('/team/:id', fetchTeam); //returns [{"name":"Hendrik"},{"name":"Natasha"},...]
+router.get('/team/:id', fetchTeam); //returns [{"name":"Hendrik"},{"name":"Wout"},...]
 router.put('/team/:id/:rider', addRider); //currently both id numbers(userId, riderId)
 router.put('/team/:rider', removeRider);
+
 router.all('*', (req, res) => res.status(404).send('Does not exist'));
 
 export default router;
