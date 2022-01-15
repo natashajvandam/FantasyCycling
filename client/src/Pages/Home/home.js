baf9df11ad10c702eec295ae1c5c1283154a7320
Natasha <natashajvandam@outlook.com>
2022-01-15 16:56:38 +0100

    can see user info on login

--- client/src/Pages/Home/home.js
+++ client/src/Pages/Home/home.js
@@ -3,7 +3,11 @@ import List from '../../Components/list/list';
 import Header from '../../Components/header/header';
 import Form from '../../Components/form/form';
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 function Home ({riderList, myRoster, addToRoster, removeFromRoster, userData, setSearchList, searchList}) {
+
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
   const filterList = (query) => {
     if (query) {
@@ -12,18 +16,23 @@ function Home ({riderList, myRoster, addToRoster, removeFromRoster, userData, se
     } 
   } 
 
+  if (isLoading) {
+    return <div>loading...</div>
+  }
+
   return (
+    (isAuthenticated &&
     <div className="home_page"> 
     <div>
     <Header 
         userData={userData}
         link_route={'league'}
       />
-
     </div>
     <div className="body_home_page">
       <div className="my_rider_list">
         <h1 className="list_title">{userData.team_name}</h1>
+        {/* <h2>{user.email}</h2> */}
         <List 
           mine={true}
           riderList={myRoster}
@@ -47,6 +56,7 @@ function Home ({riderList, myRoster, addToRoster, removeFromRoster, userData, se
 
     </div>
     </div>
+    )
   )
 }
 
