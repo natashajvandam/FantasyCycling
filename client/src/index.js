7112d7bfe1a9e8b9b24c1e70979df2f0a5affb75
Guillem Sardà <gsardaparreu@gmail.com>
2022-01-20 16:21:56 +0100

    feat: Change Autho config

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-    <React.StrictMode>
-      <Auth0Provider
-      domain="dev-874owraq.us.auth0.com"
-      clientId="HDmFTvFE2h0tUm4r0N1qzUbMB6Ck1BqA"
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-sfbx-116.us.auth0.com"
+      clientId="huLpHGSwa4GNH5DX4rH9sUlGMf3NKQxg"
       redirectUri="http://localhost:3000/home"
-      audience="https://dev-874owraq.us.auth0.com/api/v2/"
+      audience="https://dev-sfbx-116.us.auth0.com/api/v2/"
       scope="read:current_user update:current_user_metadata"
-      >
+    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      </Auth0Provider>
-    </React.StrictMode>,
-  
-  document.getElementById('root')
+    </Auth0Provider>
+  </React.StrictMode>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
