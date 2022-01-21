ad63bac8233d22ada243d854e7e966ce0f4bcde7
Guillem Sardà <gsardaparreu@gmail.com>
2022-01-21 20:13:25 +0100

    test: Add more waiting time

--- cypress/integration/Tests/untitled.spec.js
+++ cypress/integration/Tests/untitled.spec.js
@@ -5,53 +5,53 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
-describe("Welcome Page.", () => {
-  it("Successfully loads.", () => {
-    cy.visit("http://localhost:3000/"); // change URL to match your dev URL
+describe('Welcome Page.', () => {
+  it('Successfully loads.', () => {
+    cy.visit('http://localhost:3000/'); // change URL to match your dev URL
   });
-  it("Successfully log in.", () => {
-    cy.contains("Log In").click();
+  it('Successfully log in.', () => {
+    cy.contains('Log In').click();
 
-    cy.get("input[name=username]").type("santi@hotmail.com ");
-    cy.get("input[name=password]").type("hello123.");
-    cy.get("button[name=action]").click();
+    cy.get('input[name=username]').type('santi@hotmail.com ');
+    cy.get('input[name=password]').type('hello123.');
+    cy.get('button[name=action]').click();
   });
 });
 
-describe("Buys and sells a player and displays the league score.", () => {
-  it("Should buy a player.", () => {
+describe('Buys and sells a player and displays the league score.', () => {
+  it('Should buy a player.', () => {
     cy.wait(700);
-    cy.contains("buy").click();
+    cy.contains('buy').click();
     cy.wait(500);
   });
 
-  it("Should sell a player.", () => {
-    cy.wait(500);
-    cy.get("button[name=sell]").click();
-    cy.wait(500);
-  });
-
-  it("Should buy more than one player.", () => {
-    cy.wait(500);
-    cy.get("#2").contains("buy").click();
-    cy.wait(500);
-    cy.get("#30").contains("buy").click();
+  it('Should sell a player.', () => {
+    cy.wait(700);
+    cy.get('button[name=sell]').click();
     cy.wait(500);
   });
 
-  it("Should sell all the players.", () => {
-    cy.get("[id=2][class=button_myRider]").find("button[name=sell]").click();
+  it('Should buy more than one player.', () => {
     cy.wait(500);
-    cy.get("[id=30][class=button_myRider]").find("button[name=sell]").click();
+    cy.get('#2').contains('buy').click();
+    cy.wait(500);
+    cy.get('#30').contains('buy').click();
+    cy.wait(500);
   });
 
-  it("Should display the league score.", () => {
-    cy.get(".link_league").contains("league").click();
+  it('Should sell all the players.', () => {
+    cy.get('[id=2][class=button_myRider]').find('button[name=sell]').click();
+    cy.wait(500);
+    cy.get('[id=30][class=button_myRider]').find('button[name=sell]').click();
+  });
+
+  it('Should display the league score.', () => {
+    cy.get('.link_league').contains('league').click();
   });
 });
 
-describe("Succesffully logs out.", () => {
-  it("Should log out", () => {
-    cy.contains("Log Out").click();
+describe('Succesffully logs out.', () => {
+  it('Should log out', () => {
+    cy.contains('Log Out').click();
   });
 });
