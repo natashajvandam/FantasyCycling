6cb38cbf793211c0d947ff1f9b3ea9a51f6b7919
eduwp90 <eduwp90@gmail.com>
2022-01-24 21:29:04 +0100

    server: Converted user.model to TS

--- server-ts/models/user.model.ts
+++ server-ts/models/user.model.ts
@@ -1,9 +1,10 @@
 'use strict';
 
+import { QueryResult } from 'pg';
 import client from './index.model.js';
 
-const setNewUser = async (user) => {
-  const res = await client.query(`
+const setNewUser = async (user: NewUser) => {
+  const res: QueryResult = await client.query(`
     INSERT INTO user_table (email, nickname)
     VALUES ('${user.email}', '${user.nickname}')
     ON CONFLICT (nickname) DO NOTHING
@@ -11,17 +12,17 @@ const setNewUser = async (user) => {
   return res.rows.length ? res.rows[0] : res;
 };
 
-const getUserDetails = async (nickname, email) => {
-  const res = await client.query(
+const getUserDetails = async (nickname: string, email: string) => {
+  const res: QueryResult = await client.query(
     `SELECT id, email, nickname, score, money FROM user_table WHERE nickname = '${nickname}' AND email = '${email}';`
   );
   console.log('model', res);
   return res.rows[0];
 };
 
-const getUserRoster = async (user) => {
+const getUserRoster = async (user: string) => {
   if (user) {
-    const res = await client.query(
+    const res: QueryResult = await client.query(
       `SELECT * FROM rider_table WHERE roster = ${user};`
     );
     return res.rows; //rows necessary for function reliant on it.
@@ -29,12 +30,14 @@ const getUserRoster = async (user) => {
 };
 
 const fetchAllRiders = async () => {
-  const res = await client.query(`SELECT * FROM rider_table WHERE price > 10;`); //temporary 'WHERE' statement to help load faster
+  const res: QueryResult = await client.query(
+    `SELECT * FROM rider_table WHERE price > 10;`
+  ); //temporary 'WHERE' statement to help load faster
   return res.rows;
 };
 
 const fetchAllUsers = async () => {
-  const res = await client.query(`SELECT * FROM user_table;`);
+  const res: QueryResult = await client.query(`SELECT * FROM user_table;`);
   return res.rows;
 };
 
--- server-ts/types/types.ts
+++ server-ts/types/types.ts
@@ -25,3 +25,13 @@ interface RiderDates {
   end_date: Date;
   start_date: Date;
 }
+
+interface NewUser {
+  nickname: string;
+  name: string;
+  picture: string;
+  updated_at: string;
+  email: string;
+  email_verified: boolean;
+  sub: string;
+}
