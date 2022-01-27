6f92cc2549cb2c6eca7bc1622cad4e51d249158e
eduwp90 <eduwp90@gmail.com>
2022-01-27 12:11:12 +0100

    converted index.js to TS

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH0_CLIENTID}
+      domain={process.env.REACT_APP_AUTH0_DOMAIN || ''}
+      clientId={process.env.REACT_APP_AUTH0_CLIENTID || ''}
       redirectUri={process.env.REACT_APP_AUTH0_REDIRECT_URL}
       audience={`https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`}
       scope='read:current_user update:current_user_metadata'
@@ -23,8 +22,3 @@ ReactDOM.render(
 
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
